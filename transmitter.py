5484468ed6b311187d8a39285a3ca21701e2e2a8
Christos Giachoudis <giachoud@gapps.auth.gr.com>
2023-03-28 17:36:19 +0300

    transmitter py file

---
+++ transmitter.py
@@ -0,0 +1,24 @@
+from time import sleep
+import numpy as np
+import gpiozero
+
+# create the message and convert it (if needed) to 0 and 1
+message = np.zeros(40)
+for i in range(message.size):
+  if (i % 2) == 0:
+    message[i] = 1
+print(message)
+
+# declare the connection (you have to connect the led to a GPconnection)
+led = gpiozero.LED(17)
+
+space_time = 1
+# send the message
+led.on() #beginning signal
+sleep(5)
+for i in range(message.size):
+  if message[i] == 1:
+    led.on()
+  else:
+    led.off()
+  sleep(space_time)
