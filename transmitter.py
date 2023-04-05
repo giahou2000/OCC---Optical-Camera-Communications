c1f5d585e465e29d0328aaeffad1e705d0f8decf
Christos Giachoudis <giachoud@gapps.auth.gr.com>
2023-04-05 09:36:11 +0300

    failed experiment

--- transmitter.py
+++ transmitter.py
@@ -2,8 +2,11 @@ from time import sleep
 import numpy as np
 import gpiozero
 
+
+start_sequence = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+end_sequence = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 # create the message and convert it (if needed) to 0 and 1
-message = np.zeros(1000000)
+message = np.zeros(50000)
 for i in range(message.size):
   if (i % 2) == 0:
     message[i] = 1
@@ -13,11 +16,22 @@ for i in range(message.size):
 led = gpiozero.LED(17)
 
 space_time = 0.0003
+led.off()
 # send the message
-led.on() #beginning signal
-sleep(5)
+for i in range(len(start_sequence)):
+  if start_sequence[i] == 1:
+    led.on()
+  else:
+    led.off()
+  sleep(space_time)
 for i in range(message.size):
   if message[i] == 1:
+    led.on()
+  else:
+    led.off()
+  sleep(space_time)
+for i in range(len(end_sequence)):
+  if end_sequence[i] == 1:
     led.on()
   else:
     led.off()
