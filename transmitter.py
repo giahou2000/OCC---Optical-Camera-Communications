7d08d8dd8726b3afd4592d6f855ac47aeaccf98d
Christos Giachoudis <giachoud@gapps.auth.gr.com>
2023-04-25 11:04:45 +0300

    transmitter ready for test

--- transmitter.py
+++ transmitter.py
@@ -2,9 +2,6 @@ from time import sleep
 import numpy as np
 import gpiozero
 
-
-start_sequence = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
-end_sequence = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 # create the message and convert it (if needed) to 0 and 1
 message = np.zeros(1000000)
 for i in range(message.size):
@@ -12,26 +9,18 @@ for i in range(message.size):
     message[i] = 1
 # print(message)
 
-# declare the connection (you have to connect the led to a GPconnection)
+# declare the connection (you have to connect the led to a GPIO connection)
 led = gpiozero.LED(17)
 
 space_time = 0.0003
 led.off()
 # send the message
-for i in range(len(start_sequence)):
-  if start_sequence[i] == 1:
-    led.on()
-  else:
-    led.off()
-  sleep(space_time)
+led.on()
+sleep(10)
+led.off()
+sleep(5)
 for i in range(message.size):
   if message[i] == 1:
-    led.on()
-  else:
-    led.off()
-  sleep(space_time)
-for i in range(len(end_sequence)):
-  if end_sequence[i] == 1:
     led.on()
   else:
     led.off()
