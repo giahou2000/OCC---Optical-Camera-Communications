dae4f0a665a82c9dd8cd4d2c74911935deda4a1d
Christos Giachoudis <giachoud@gapps.auth.gr.com>
2023-05-04 11:45:56 +0300

    Found out that we loose 45 bits per frame change

---
+++ small_message.py
@@ -0,0 +1,27 @@
+from time import sleep
+import numpy as np
+import gpiozero
+
+# create the message and convert it (if needed) to 0 and 1
+message = np.zeros(500)
+for i in range(message.size):
+  if (i % 2) == 0:
+    message[i] = 1
+# print(message)
+
+# declare the connection (you have to connect the led to a GPIO connection)
+led = gpiozero.LED(17)
+
+space_time = 0.001
+led.off()
+# send the message
+led.on()
+sleep(10)
+led.off()
+sleep(5)
+for i in range(message.size):
+  if message[i] == 1:
+    led.on()
+  else:
+    led.off()
+  sleep(space_time)
